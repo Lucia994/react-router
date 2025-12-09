97b48e1a32cad904bb1f8d1b9d3077e45f337ab7
Lucia994 <l.summa94@gmail.com>
2025-12-09 18:13:44 +0100

    Create DefaulLayout and import Header and Footer components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,29 @@
 import { NavLink } from "react-router-dom";
+const navLinks = [
+  {
+    title: "HomePage",
+    path: "/",
+  },
+  {
+    title: "About us",
+    path: "/about-us",
+  },
+  {
+    title: "Products",
+    path: "/products",
+  },
+];
 
 export default function Header() {
-  return <></>;
+  return (
+    <header>
+      <ul>
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <NavLink to={link.path} element={link.title} />
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 }
