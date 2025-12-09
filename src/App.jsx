4f7c87b3914b86055537ad2e74a4021a731a058c
Lucia994 <l.summa94@gmail.com>
2025-12-09 16:21:57 +0100

    Create Homepage, About us and products pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import Products from "./pages/Products";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
